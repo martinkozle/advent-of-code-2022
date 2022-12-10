512371ac995ef9067f64215adf486167ce8df644
Martin Popovski <martinkozle@yahoo.com>
2022-12-10 15:13:40 +0100

    Add solution for day 10

---
+++ src/days/day_10/part1.rs
@@ -0,0 +1,25 @@
+pub fn solve(input: String) -> String {
+    let mut cycle = 0;
+    let mut x = 1;
+    let mut sum = 0;
+    for line in input.lines() {
+        cycle += 1;
+        if cycle == 20 || (cycle - 20) % 40 == 0 {
+            sum += cycle * x;
+        }
+        match line {
+            "noop" => {}
+            other => match other.split_once(' ') {
+                Some(("addx", amount)) if amount.parse::<i32>().is_ok() => {
+                    cycle += 1;
+                    if cycle == 20 || (cycle - 20) % 40 == 0 {
+                        sum += cycle * x;
+                    }
+                    x += amount.parse::<i32>().unwrap();
+                }
+                _ => panic!("Unexpected input command"),
+            },
+        };
+    }
+    sum.to_string()
+}
---
+++ src/days/day_10/part2.rs
@@ -0,0 +1,41 @@
+use itertools::Itertools;
+
+type Crt = [[char; 40]; 6];
+
+trait Drawable {
+    fn draw(&mut self, sprite_location: i32, cycle: usize);
+}
+
+impl Drawable for Crt {
+    fn draw(&mut self, sprite_location: i32, cycle: usize) {
+        let row: usize = (cycle - 1) / 40;
+        let col: usize = (cycle - 1) % 40;
+        if (sprite_location - col as i32).abs() <= 1 {
+            self[row][col] = '#';
+        }
+    }
+}
+
+pub fn solve(input: String) -> String {
+    let mut cycle: usize = 0;
+    let mut x: i32 = 1;
+    let mut crt: Crt = [['.'; 40]; 6];
+    for line in input.lines() {
+        cycle += 1;
+        crt.draw(x, cycle);
+        match line {
+            "noop" => {}
+            other => match other.split_once(' ') {
+                Some(("addx", amount)) if amount.parse::<i32>().is_ok() => {
+                    cycle += 1;
+                    crt.draw(x, cycle);
+                    x += amount.parse::<i32>().unwrap();
+                }
+                _ => panic!("Unexpected input command"),
+            },
+        };
+    }
+    crt.iter()
+        .map(|row| row.iter().collect::<String>())
+        .join("\n")
+}
